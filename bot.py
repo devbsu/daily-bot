c957d983d54db2e266f19405b555d0faf3e18f75
devbsu <devbsu@naver.com>
2025-08-12 03:04:11 +0900

    feat: add discord daily thread bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import os
+from discord.ext import commands
+from datetime import datetime
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+token = os.getenv("DISCORD_TOKEN")
+channel_id = 1404434756388524160  # 채널 ID
+
+already_ran = False
+
+@bot.event
+async def on_ready():
+    global already_ran
+    if already_ran:
+        return
+    already_ran = True 
+
+    now = datetime.now()
+    channel = bot.get_channel(channel_id)
+
+    if channel:
+        embed = discord.Embed(
+            title=now.strftime('%m월 %d일'),
+            description="내용은 스레드를 통해 작성해 주세요.",
+            color=discord.Color.orange()
+        )
+
+        main_message = await channel.send(embed=embed)
+
+        # 스레드 생성
+        await main_message.create_thread(
+            name=now.strftime('%m월 %d일'),
+            auto_archive_duration=1440
+        )
+
+    await bot.close()
+
+bot.run(token)
